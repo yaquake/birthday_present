7046707f0d291300bfd334d6c8ae68cebd18420b
Ivan <ivan@programmer.net>
2019-03-06 13:31:21 +1300

    optimizing

--- Alinka/urls.py
+++ Alinka/urls.py
@@ -10,4 +10,7 @@ urlpatterns = [
     path('page_<int:page>', views.pages, name='pages'),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+
